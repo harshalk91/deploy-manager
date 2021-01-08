1924d3d1260727b0964683ced58569e20e17ca6b
Harshal Kulkarni <harshal.kulkarni@amdocs.com>
2021-01-08 06:47:50 +0200

    Code restructuring

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request, redirect
 from database import database
 from utils import get_uuid, get_current_timestamp
 from flask_api import status
-from workflow import *
 from flask_celery import make_celery
 import time
+from deployment import *
 
 logging.basicConfig(
     filename="deploymanager-workflow",
@@ -19,30 +19,22 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379',
+    CELERY_BROKER_URL='redis://10.247.155.80:6379',
+    CELERY_RESULT_BACKEND='redis://10.247.155.80:6379',
     CELERY_TASK_SERIALIZER='json',
     CELERY_RESULT_SERIALIZER='json'
 )
+
 celery = make_celery(app)
 celery_trigger_deployment = make_celery(app)
-celery_2 = make_celery(app)
 
 
 @app.route("/getdeployments", methods=['GET'])
 def getDeployments():
     if request.method == "GET":
         collection = "deployment"
-        logger.debug("Initializing DB")
-        database.initialize()
-        logger.debug("Initialized DB")
-        logger.debug("Getting results from DB")
-        result = database.getAllDeployments(collection)
-        logging.debug(result)
-        re = []
-        for i in result:
-            logger.debug(i)
-            re.append(i)
+        deployment_obj = deployment()
+        re = deployment_obj.get_deployments(collection)
         if not re:
             return status.HTTP_500_INTERNAL_SERVER_ERROR
         else:
@@ -67,94 +59,37 @@ def createDeployment():
 
     if request.method == "POST":
         logger.debug("Request Came for inserting data")
-        data = request.get_json()
-        data['start_time'] = get_current_timestamp()
-        data['end_time'] = ""
-        data['user'] = "Harshal"
-        data['status'] = "InProgress"
-        data['deployment_id'] = get_uuid()
         collection = "deployment"
-        logger.debug("Initializing DB")
-
-        insertToDB.delay(collection, data)
-        query = {"deployment_id": data['deployment_id']}
-        database.initialize()
-        time.sleep(1)
-        result = database.getData(collection, query)
-        # cloud_credentials = getCloudCredentials(data['cloud_provider'])
-        logger.debug("Triggering Async Task to create deployment")
+        deployment_obj = deployment()
+        deployment_json = deployment_obj.create_json(request.get_json())
+        deployment.insertToDB(collection, deployment_json)
+        result = deployment_obj.get_data(collection, deployment_json['deployment_id'])
         celeryTriggerDeployment.apply_async(
-            args=[data['name'], data['template'], data['instance_count'], collection, data['deployment_id'],
-                  data['cloud_provider']], countdown=2, expires=180)
+            args=[deployment_json, collection], countdown=2, expires=180)
         return render_template('show_deployments.html',
-                               title='overview',
-                               result=result), status.HTTP_200_OK
-
-
-@celery.task(name='celery_example.celery')
-def insertToDB(collection, data):
-    logger.debug("Async Task Started for Inserting deployment record into DB")
-    database.initialize()
-    database.insert(collection, data)
-
-
-@celery.task(name='celery_example.celery_trigger_deployment', serializer='json')
-def celeryTriggerDeployment(name, template, instance_count, collection, deployment_id, cloud_provider):
-    logger.debug("Async Task Started for Triggering Deployment")
-    database.initialize()
-    logger.debug("Database initialized")
-    cloud_credentials = getCloudCredentials(cloud_provider)
-    logger.debug(cloud_credentials)
-    logger.debug("Triggering deployment for %s", name)
-    template_data = {
-        "aws_access_key": cloud_credentials[0]['aws_access_key'],
-        "aws_secret_key": cloud_credentials[0]['aws_secret_key'],
-        "aws_region": cloud_credentials[0]['aws_region'],
-        "ami": cloud_credentials[0]['ami'],
-        "instance_count": instance_count,
-        "instance_type": cloud_credentials[0]['template'][template],
-        "key_name": "jumpbox-kepair",
-        "subnet_id": "subnet-022ab974e8cce7e1d",
-        "security_group_id": "sg-0639f1fc8e91af47e",
-        "instance_name": name
-    }
-    tfvars_file = jinjaLoader(template_data, deployment_id)
-    logger.debug(tfvars_file)
-    if os.path.exists(tfvars_file):
-
-        deploy_id = {"deployment_id": deployment_id}
-        query = {"$set": {'status': 'Instance Creation Started"'}}
-        database.updateone(collection, deploy_id, query)
-        logger.debug("DB Updated and deployment status changed")
-
-        terraform_dir = os.path.join(os.getcwd() + "/aws-terraform")
-        logger.debug("Instance Created Started")
-        inst_status = createInstancetf(terraform_dir, collection, deployment_id, tfvars_file)
-        #logger.debug(inst_status)
-    else:
-        logger.error("Error!! File Does Not exist")
-        return "Error"
+                                title='overview',
+                                result=result), status.HTTP_200_OK
 
 
 @app.route("/status", methods=['GET'])
 def get_deployment_status():
     if request.method == "GET":
-        logger.debug("Request came for /status/")
-        data = request.args.get("deployment_id")
-        logger.debug("data: %s", data)
-        query = dict(deployment_id=data)
+        deployment_obj = deployment()
+        query = dict(deployment_id=request.args.get("deployment_id"))
         collection = "deployment"
-        logger.debug("Initializing DB")
-        database.initialize()
-        logger.debug("Initialized DB")
-        logger.debug("Getting Deployment Status for %s", data)
-        result = database.getData(collection, query)
+        result = deployment_obj.get_deployment_status(collection, query)
         if not result:
             return status.HTTP_500_INTERNAL_SERVER_ERROR
         else:
             return render_template('show_deployments.html',
                                    title='overview',
                                    result=result)
+
+
+@celery.task(name='celery_example.celery_trigger_deployment', serializer='json')
+def celeryTriggerDeployment(deployment_json, collection):
+    logger.debug("Hello from celeryTriggerDeployment")
+    #deployment.celeryTriggerDeployment(deployment_json, collection)
 
 
 if __name__ == '__main__':
---
+++ deployment.py
@@ -0,0 +1,173 @@
+from utils import *
+from database import *
+import logging
+from jinja2 import Template
+import os
+from python_terraform import *
+from shutil import copyfile
+
+logging.basicConfig(
+    filename="deploymanager-workflow",
+    filemode='a',
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
+logging.info("Started...")
+logger = logging.getLogger(__name__)
+
+
+class deployment:
+
+    def create_json(self, data):
+        data['start_time'] = get_current_timestamp()
+        data['end_time'] = ""
+        data['user'] = "Harshal"
+        data['status'] = "InProgress"
+        data['deployment_id'] = get_uuid()
+        return data
+
+    def get_data(self, collection, deployment_id):
+        query = {"deployment_id": deployment_id}
+        return database.getData(collection, query)
+
+    def get_deployments(self, collection):
+        database.initialize()
+        result = database.getAllDeployments(collection)
+        re = []
+        for i in result:
+            logger.debug(i)
+            re.append(i)
+        return re
+
+    def get_deployment_status(self, collection, query):
+        logger.debug("Request came for /status/")
+
+        logger.debug("Initializing DB")
+        database.initialize()
+        logger.debug("Initialized DB")
+        logger.debug("Getting Deployment Status for %s", data)
+        result = database.getData(collection, query)
+        return result
+
+    def getCloudCredentials(self, cloud_provider):
+        logger.debug("Fetching DB Credentials")
+        database.initialize()
+        collection = "cloudvault"
+        query = {"profile": "default", "cloud": "aws"}
+        result = database.getData(collection, query)
+        re = []
+        for i in result:
+            re.append(i)
+        return re
+
+    def jinjaLoader(self, template_data, deployment_id):
+        """
+
+        :param template_data: Terraform tfvars in json format
+        :param deployment_id:
+        :return:
+        """
+        logger.debug("Copying template")
+        original_file = os.path.join(os.getcwd(), "aws-terraform/terraform.tfvars.j2")
+        newfile = os.path.join(os.getcwd(), "aws-terraform/" + deployment_id + "-terraform.tfvars")
+        copyfile(original_file, newfile)
+        logger.debug("Rendering template")
+        with open(newfile, "r+") as f:
+            data = f.read()
+            j2_template = Template(data)
+            f.seek(0)
+            f.write(j2_template.render(template_data))
+            f.truncate()
+        return newfile
+
+    def createInstanceTF(self, terraform_path, collection, deployment_id, tfvars_file):
+        """
+
+        :param terraform_path: Terraform Directory absolute path
+        :param collection: deployment collection name
+        :param deployment_id: deployment_id generated from create_json function
+        :param tfvars_file: absolute path for terraform variable file
+        :return:
+        """
+        try:
+            database.initialize()
+            deploy_id = {"deployment_id": deployment_id}
+            tf = Terraform(working_dir=terraform_path, var_file=tfvars_file)
+
+            return_code, stdout, stderr = tf.init()
+            if stderr:
+                query = {"$set": {'status': 'Terraform Initialization Failed'}}
+                database.updateone(collection, deploy_id, query)
+                logger.debug(stderr)
+                raise ValueError('Terraform Initialization Failed')
+            else:
+                query = {"$set": {'status': 'Terraform Initialization Complete'}}
+                database.updateone(collection, deploy_id, query)
+
+            return_code, stdout, stderr = tf.cmd("apply", "-state=" + deployment_id + ".tfstate",
+                                                 "-state-out=" + deployment_id + ".tfstate", "-var-file=" + tfvars_file,
+                                                 "-auto-approve")
+            # return_code, stdout, stderr = tf.apply(no_color=IsFlagged, refresh=False, skip_plan=True)
+            if return_code != 0:
+                query = {"$set": {'status': 'Terraform Apply Failed'}}
+                database.updateone(collection, deploy_id, query)
+                logger.debug(stderr)
+                raise ValueError('Terraform Apply Failed')
+            else:
+                query = {"$set": {'status': 'Terraform Apply Successful'}}
+                database.updateone(collection, deploy_id, query)
+
+                tfstate_file = terraform_path + "/" + deployment_id + ".tfstate"
+                with open(tfstate_file, 'r') as tfstate_obj:
+                    tfstate_json = json.loads(tfstate_obj.read())
+
+                tfstate_data = {"deployment_id": deployment_id, "tfstate": tfstate_json}
+                database.insert(collection, tfstate_data)
+
+            return "Terraform Apply Successful"
+            # read_state_file = tf.read_state_file()
+            # logger.debug(read_state_file)
+        except ValueError as err:
+            logger.debug(err.args)
+   
+    @staticmethod 
+    def insertToDB(collection, data_json):
+        database.initialize()
+        database.insert(collection, data_json)
+
+    def celeryTriggerDeployment(self, data, collection):
+        logger.debug("Async Task Started for Triggering Deployment")
+        cloud_credentials = self.getCloudCredentials(data['cloud_provider'])
+        logger.debug("Triggering deployment for %s", data['name'])
+        template_data = {
+            "aws_access_key": cloud_credentials[0]['aws_access_key'],
+            "aws_secret_key": cloud_credentials[0]['aws_secret_key'],
+            "aws_region": cloud_credentials[0]['aws_region'],
+            "ami": cloud_credentials[0]['ami'],
+            "instance_count": data['instance_count'],
+            "instance_type": cloud_credentials[0]['template'][data['template']],
+            "key_name": "jumpbox-kepair",
+            "subnet_id": "subnet-022ab974e8cce7e1d",
+            "security_group_id": "sg-0639f1fc8e91af47e",
+            "instance_name": data['name']
+        }
+        tfvars_file = self.jinjaLoader(template_data, data['deployment_id'])
+        logger.debug(tfvars_file)
+        '''
+        if os.path.exists(tfvars_file):
+
+            deploy_id = {"deployment_id": data['deployment_id']}
+            query = {"$set": {'status': 'Instance Creation Started"'}}
+            database.updateone(collection, deploy_id, query)
+            logger.debug("DB Updated and deployment status changed")
+
+            terraform_dir = os.path.join(os.getcwd() + "/aws-terraform")
+            logger.debug("Instance Created Started")
+            inst_status = self.createInstanceTF(terraform_dir, collection, data['deployment_id'], tfvars_file)
+            # logger.debug(inst_status)
+        else:
+            logger.error("Error!! File Does Not exist")
+            return "Error"
+        '''
