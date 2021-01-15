24affcae382c9d95a5492e7ee4fe39061540a293
Shrivallabh Mahajan <shrivallabh.mahajan@sas.com>
2021-01-15 18:15:22 +0530

    added mysql database ORM in code and deployments page for AWS

--- db_scripts/create_tables.sql
+++ db_scripts/create_tables.sql
@@ -9,20 +9,15 @@ CREATE TABLE `providers` (
   `enable` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `deploymanager`.`deployments` (
-  `id` VARCHAR(45) NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `description` TEXT NULL,
-  `deploymentscol` VARCHAR(45) NULL,
-  `state` VARCHAR(45) NULL,
-  `provider_id` VARCHAR(45) NULL,
-  `deploymentscol1` VARCHAR(45) NULL,
-  `created_by` VARCHAR(15) NULL,
-  `created_date` TIMESTAMP NULL,
+CREATE TABLE `deployments` (
+  `id` varchar(45) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `description` text,
+  `state` varchar(45) DEFAULT NULL,
+  `provider_id` varchar(45) DEFAULT NULL,
+  `created_by` varchar(15) DEFAULT NULL,
+  `created_date` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `deployment_provider_fk_idx` (`provider_id` ASC) VISIBLE,
-  CONSTRAINT `deployment_provider_fk`
-    FOREIGN KEY (`provider_id`)
-    REFERENCES `deploymanager`.`providers` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  KEY `deployment_provider_fk_idx` (`provider_id`),
+  CONSTRAINT `deployment_provider_fk` FOREIGN KEY (`provider_id`) REFERENCES `providers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
